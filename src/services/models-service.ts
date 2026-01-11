0f905351b0721197592613cd7dffe5984d44211e
LudmiAbril <ludmila.pereyra543@gmail.com>
2026-01-11 19:07:05 -0300

    url fix

--- src/services/models-service.ts
+++ src/services/models-service.ts
@@ -1,6 +1,6 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 export const getModels = async () => {
-    const response = await fetch(`${apiUrl}/api/models`);
+    const response = await fetch(`${apiUrl}/api/models/`);
     if (!response.ok) throw new Error("Error en la red");
     return await response.json();
 }
