93ae781adaef0110475d4f695dc617afe2b957a2
LudmiAbril <ludmila.pereyra543@gmail.com>
2026-01-11 19:11:33 -0300

    url fix

--- src/services/models-service.ts
+++ src/services/models-service.ts
@@ -5,7 +5,7 @@ export const getModels = async () => {
     return await response.json();
 }
 export const getModelDetails = async (id: string) => {
-    const response = await fetch(`${apiUrl}/api/models/${id}`);
+    const response = await fetch(`${apiUrl}/api/models/${id}/`);
     if (!response.ok) throw new Error("Error en la red");
     return await response.json();
 }
